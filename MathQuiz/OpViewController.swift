5aa409ad46e1847be50bb9c9776091f4d177dda8
Rohit Ramarathinam <rohit.ramarathinam.017@k12.friscoisd.org>
2021-03-31 14:24:38 -0500

    added second screen and started touch up connections

--- MathQuiz/OpViewController.swift
+++ MathQuiz/OpViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class OpViewController: UIViewController {
 
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        addButton.setTitleColor(.green, for: .normal)
+    }
+    
+    @IBAction func minusButtonPressed(_ sender: UIButton) {
+        minusButton.setTitleColor(.green, for: .normal)
+    }
+    
+    @IBAction func multiplyButtonPressed(_ sender: UIButton) {
+        multiplyButton.setTitleColor(.green, for: .normal)
+    }
+    
+    @IBAction func divideButtonPressed(_ sender: UIButton) {
+        divideButton.setTitleColor(.green, for: .normal)
+    }
 }
